3d75dbc733ae14a1c1ef7b7cfb2cf8620c649b23
DoubleTap347 <seandanzo@live.com>
2017-04-20 22:28:40 +0800

    First Commit
    Datagrid views aren't working yet.
    Picturebox was just added.
    Login works.
    Committed on <strong>20/04/2017</strong>

---
+++ LoginScreenForm/LoginScreenForm/LoginScreen.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LoginScreenForm
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserBox = new System.Windows.Forms.TextBox();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UserBox
+            // 
+            this.UserBox.Location = new System.Drawing.Point(79, 40);
+            this.UserBox.Name = "UserBox";
+            this.UserBox.Size = new System.Drawing.Size(184, 20);
+            this.UserBox.TabIndex = 0;
+            this.UserBox.Text = "lister";
+            this.UserBox.TextChanged += new System.EventHandler(this.UserBox_TextChanged);
+            // 
+            // PassBox
+            // 
+            this.PassBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.PassBox.Location = new System.Drawing.Point(79, 88);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.PasswordChar = '*';
+            this.PassBox.Size = new System.Drawing.Size(184, 20);
+            this.PassBox.TabIndex = 1;
+            this.PassBox.Text = "1472";
+            this.PassBox.TextChanged += new System.EventHandler(this.PassBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LoginBtn.Location = new System.Drawing.Point(188, 140);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(75, 23);
+            this.LoginBtn.TabIndex = 5;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Welcome to my app.";
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 175);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.UserBox);
+            this.Name = "LoginScreen";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoginScreen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserBox;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LoginScreenForm/LoginScreenForm/LoginScreen.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UI_on_steroids;
+
+namespace LoginScreenForm
+{
+    public partial class LoginScreen : Form
+    {
+        string username = "lister";
+        string password = "1472";
+
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if((UserBox.Text == username)&&(PassBox.Text == password))
+            {
+                MessageBox.Show("Login to the app was successful!");
+                this.Hide();
+                //Form1 ss = new Form1();
+                //Form1.Show();
+
+                DataDineFrm ss = new DataDineFrm();
+                ss.Show();
+
+
+            }
+            else {
+
+                MessageBox.Show("Login details incorrect");
+                PassBox.Text = "";
+            }
+
+            
+            //LoginScreen ss = new LoginScreen();
+            //ss.Show();
+        }
+
+        private void LoginScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PassBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
